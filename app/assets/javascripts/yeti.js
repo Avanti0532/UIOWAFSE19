fa000b0254d1229860b16c9acd5e64e9fb912834
Avanti0532 <avantiharish-deshmukh@uiowa.edu>
2019-10-02 09:54:10 -0500

    devise changes for sign/up and login

---
+++ app/assets/javascripts/yeti.js
@@ -0,0 +1,9 @@
+// Yeti
+// Bootswatch
+//= require jquery
+//= require jquery-ui
+//= require jquery_ujs
+//= require yeti/loader
+//= require yeti/bootswatch
+//= require datatables
+//= require datatables/dataTables.bootstrap4
---
+++ db/migrate/20191002133649_add_first_name_last_name_to_faculty.rb
@@ -0,0 +1,6 @@
+class AddFirstNameLastNameToFaculty < ActiveRecord::Migration
+  def change
+    add_column :faculties, :first_name, :string
+    add_column :faculties, :last_name, :string
+  end
+end
