da75a7befea1982799eebfec76feb57d30d271ad
Avanti0532 <avantiharish-deshmukh@uiowa.edu>
2019-11-18 21:13:05 -0600

    course registration changes

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,5 @@ class Course < ActiveRecord::Base
   belongs_to :faculty
   has_many :registration
   validates :course_name, :presence => true
+  validates :applicable_fee,:level,:faculty, :description, :time, :presence =>true
 end
