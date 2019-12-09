4deb407c6d3134f24116f02bf70aeb751ed20263
Avanti0532 <avantiharish-deshmukh@uiowa.edu>
2019-12-09 13:41:17 -0600

    changes for uploading assignment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -3,4 +3,8 @@ class AssignmentsController < ApplicationController
     @assignments = Assignment.all
     @course_name = params[:name]
   end
+
+  def new
+   @assignment = Assignment.new
+  end
 end
